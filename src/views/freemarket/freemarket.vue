2e0ff9e9a58607d205192b8c062c17572b759eff
jrsports <jrsports1106@163.com>
2020-02-03 20:49:13 +0800

    start freemarket

--- src/views/freemarket/freemarket.vue
+++ src/views/freemarket/freemarket.vue
@@ -34,7 +34,7 @@
                         </el-col>
                     </el-row>
                     <el-row :gutter="20">
-                        <el-col :span="8">
+                        <el-col :span="8" v-for="fp in freePlayerListData" :key="fp">
                             <div class="freePlayer">
                                 <el-row>
                                     <el-col :span="8">
@@ -42,9 +42,9 @@
                                     </el-col>
                                     <el-col :span="16">
                                         <div>
-                                            <h3>郎佐-鲍尔<span>Lonzo Ball</span></h3>
-                                            <h3>{{position}}|<span>进攻：{{offensive}}防守：{{defensive}}</span></h3>
-                                            <h4>原属球队：{{originalTeam}}</h4>
+                                            <h3>{{fp.basicPlayer.chname}}<span>{{fp.basicPlayer.enname}}</span></h3>
+                                            <h3>{{fp.basicPlayer.position}}|<span>进攻：{{fp.basicPlayer.offensive}}防守：{{fp.basicPlayer.defensive}}</span></h3>
+                                            <h4>原属球队：{{fp.source}}</h4>
                                         </div>
                                     </el-col>
                                 </el-row>
@@ -58,7 +58,7 @@
                                 </el-row>
                                 <el-row style="margin-top: 30px">
                                     <el-col :span="12" style="text-align: center">
-                                        <span>剩余时间：12:18</span>
+                                        <span>剩余时间：{{fp.timeLeft}}</span>
                                     </el-col>
                                     <el-col :span="12" style="text-align: center">
                                         <el-button type="text" @click="offerDialogVisible = true">报价情况</el-button>
@@ -74,6 +74,7 @@
                             <el-table-column type="index" label="排名" width="50"></el-table-column>
                             <el-table-column property="teamName" label="球队"></el-table-column>
                             <el-table-column property="offer" label="报价"></el-table-column>
+                            <el-table-column property="offerTime" label="报价时间"></el-table-column>
                             <el-table-column property="intention" label="意向"></el-table-column>
                         </el-table>
                     </el-dialog>
@@ -101,6 +102,7 @@
                 originalTeam:"originalTeam",
                 position:"PG",
                 offerDialogVisible:false,
+                lastFpId:-1,
                 positionFilterOptions: [{
                     value: '1',
                     label: 'PG（控球后卫）'
@@ -122,14 +124,89 @@
                     {
                         teamName:"koliday",
                         offer:"3年2500万",
+                        offerTime:"2020/2/3 16:14:18",
                         intention:"97%"
                     }
-                ]
+                ],
+                freePlayerListData:[]
             }
 
         },
         mounted(){
             this.teamName=sessionStorage.getItem("teamName");
+            this.refreshFreePlayer();
+            setInterval(this.startTimer,1000);
+        },
+        methods:{
+            getMoreFreePlayer(){
+                const me = this;
+                this.axios.post("http://www.jrsports.com/api/freemarket/free/getFreePlayerList", {
+                    fpIdOffset:this.lastFpId,
+                    count:9
+                }, {
+                    headers: {
+                        "userToken": localStorage.getItem("userToken"),
+                        "teamToken": sessionStorage.getItem("teamToken")
+                    }
+                }).then(function (response) {
+                    const freeResponse = response.data;
+                    if (freeResponse.code === 0) {
+                        const newFreePlayerList=freeResponse.data;
+                        newFreePlayerList.forEach(function (item) {
+                            item.timeLeft=me.secondsToTime((item.expireTimeStamp-new Date().getTime())/1000)
+                            me.freePlayerListData.push(item)
+                        });
+                    } else {
+                        me.$message({
+                            message: freeResponse.msg,
+                            type: "warning"
+                        });
+                    }
+                });
+            },
+            refreshFreePlayer(){
+                const me = this;
+                this.axios.post("http://www.jrsports.com/api/freemarket/free/getFreePlayerList", {
+                    fpIdOffset:this.lastFpId,
+                    count:9
+                }, {
+                    headers: {
+                        "userToken": localStorage.getItem("userToken"),
+                        "teamToken": sessionStorage.getItem("teamToken")
+                    }
+                }).then(function (response) {
+                    const freeResponse = response.data;
+                    if (freeResponse.code === 0) {
+                        const newFreePlayerList=freeResponse.data;
+                        me.freePlayerListData=[];
+                        newFreePlayerList.forEach(function (item) {
+                            item.timeLeft=me.secondsToTime(parseInt((item.expireTimeStamp-new Date().getTime())/1000));
+                            me.freePlayerListData.push(item)
+                        });
+                    } else {
+                        me.$message({
+                            message: freeResponse.msg,
+                            type: "warning"
+                        });
+                    }
+                });
+            },
+            secondsToTime(s) {
+                let h;
+                h = Math.floor(s / 60);
+                s = s % 60;
+                h += '';
+                s += '';
+                h = (h.length == 1) ? '0' + h : h;
+                s = (s.length == 1) ? '0' + s : s;
+                return h + ':' + s;
+            },
+            startTimer(){
+                const me=this;
+                this.freePlayerListData.forEach(function (item) {
+                    item.timeLeft=me.secondsToTime(parseInt((item.expireTimeStamp-new Date().getTime())/1000));
+                });
+            }
         }
     }
 </script>
